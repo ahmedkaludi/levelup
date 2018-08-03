60f7a745fb5f9e34f487d4d71d79e0c39a3bbaf3
santosh8793 <40556903+santosh8793@users.noreply.github.com>
2018-08-03 12:29:58 +0530

    elementor api files

---
+++ elementor-layouts/layouts/design1-layout/amp-layout.php
@@ -0,0 +1,11 @@
+<?php
+$amp_css = '';
+$amp_markup = array(
+				'amp_html'=> '<div id="amp-call-to-action-design1" class="design-layout-default" role="default" aria-label="default">
+						<h1>Design 1</h1>
+						<div class="amp-img-container-design1"><img src="{{image}}" ></div>
+						<h3 class="elementor-inline-editing amp-title" data-elementor-setting-key ="title" data-elementor-call-to-action-toolbar="basic" >{{title}}</h3>
+						</div>',
+				'amp_css' => $amp_css
+);
+return $amp_markup;
