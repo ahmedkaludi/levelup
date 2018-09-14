d9235b638323df842d6c4e5a3bf0b2ac58cd53ee
Ajeet kumar <ajeet.gupta51@gmail.com>
2018-09-14 12:24:10 +0530

    Check entered API is valid or not before check for update available

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -31,8 +31,9 @@ Class LEVELUP__Admin_settings{
 	}
 
 	public function levelup_settings_menu(){
+		$settings = get_option('levelup_library_settings');
 		$availableUpdateHtml = '';
-		if($this->check_update_available()){
+		if($settings['api_status']=='valid' && $this->check_update_available()){
 			$availableUpdateHtml = '<span class="update-plugins count-1"><span class="update-count" title="">'.esc_html__( '1', LEVELUP_TEXT_DOMAIN ).'</span></span>';
 		}
 			$menu_label = sprintf( esc_html__( 'LevelUp ', LEVELUP_TEXT_DOMAIN)." %s",  $availableUpdateHtml);
