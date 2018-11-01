43725a01dfbd46ba9db9927905e03b81110b1d1a
Raju <rajujeelaga@gmail.com>
2018-11-01 13:56:51 +0530

    New Testimonial design fields are done with slider
    New Testimonial design fields are done with slider

--- assets/js/frontend/mainlightslider.js
+++ assets/js/frontend/mainlightslider.js
@@ -2,18 +2,10 @@
 jQuery(document).ready(function($) {
 		$("#content-slider").lightSlider({
             loop:true,
-            keyPress:true
-        });
-        $('#image-gallery').lightSlider({
-            gallery:true,
+            keyPress:true,
             item:1,
-            thumbItem:9,
-            slideMargin: 0,
-            speed:500,
             auto:false,
-            loop:true,
-            onSliderLoad: function() {
-                $('#image-gallery').removeClass('cS-hidden');
-            }  
+            pager: false,
         });
+
 	});
--- widgets/testimonial-widget.php
+++ widgets/testimonial-widget.php
@@ -67,20 +67,20 @@ class TestimonialWidgets extends Widget_Base {
 	 */
 	public function get_script_depends() {
 		$scriptArray = array();
-		$settings = $this->get_settings();
-		if($settings['layoutDesignSelected']=='testimonial-design-1'){
+		/*$settings = $this->get_settings();
+		if(isset($settings['layoutDesignSelected']) && $settings['layoutDesignSelected']=='testimonial-design-1'){
+		}*/
 			$scriptArray = array('levelup-testimonial-widget', 'levelup-testimonial-widget-slider');
-		}
 		return $scriptArray;
 	}
 
 	public function get_style_depends() {
 		$styleArray = array();
-		$settings = $this->get_settings();
-		if($settings['layoutDesignSelected']=='testimonial-design-1'){
-			$styleArray = array('levelup-testimonial-widget');
-		}
-		return $styleArray;
+		/*$settings = $this->get_settings();
+		if(isset($settings['layoutDesignSelected']) && $settings['layoutDesignSelected']=='testimonial-design-1'){
+		}*/
+			$styleArray = array('levelup-testimonial-widget-style');
+		return ['levelup-testimonial-widget-style'];
 	}
 	/**
 	 * Register the widget controls.
@@ -128,9 +128,80 @@ class TestimonialWidgets extends Widget_Base {
 			)
 		);
 
-		// Team Design 1 Fileds//
-		 
-		
+		// Testimonial Design 1 Fileds//
+		$repeater1 = new \Elementor\Repeater();
+
+		$repeater1->add_control(
+			'tst1-image',
+			[
+				'label' => esc_html__( 'Choose Image', LEVELUP_TEXT_DOMAIN ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
+			]
+		);
+		$repeater1->add_control(
+			'tst1-head', [
+				'label' => esc_html__( 'Heading', LEVELUP_TEXT_DOMAIN ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Text' , LEVELUP_TEXT_DOMAIN ),
+				'label_block' => true,
+			]
+		);
+		$repeater1->add_control(
+			'tst1-desc', [
+				'label' => esc_html__( 'Description', LEVELUP_TEXT_DOMAIN ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'desc' , LEVELUP_TEXT_DOMAIN ),
+				'label_block' => true,
+			]
+		);
+		$repeater1->add_control(
+			'tst1-athr', [
+				'label' => esc_html__( 'Author', LEVELUP_TEXT_DOMAIN ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Text' , LEVELUP_TEXT_DOMAIN ),
+				'label_block' => true,
+			]
+		);
+		$this->add_control(
+			'testi1-rep',
+			[
+				'label' => esc_html__( 'Repeater List', LEVELUP_TEXT_DOMAIN ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => $repeater1->get_controls(),
+				'default' => [
+					[
+						'tst1-image' => esc_html__( 'http://levelup.magazine3.company/wp-content/uploads/2018/10/tm2-5.jpg', LEVELUP_TEXT_DOMAIN ),
+						'tst1-head'=> esc_html__( 'New Providence is the great UI Kit', LEVELUP_TEXT_DOMAIN ),
+						'tst1-desc'=> esc_html__( 'Just then her head struck aganist the roof of the hall:in fact she was now more than nine feet high, and she at once at once took up the little gilden key and hurried off to the garden door.', LEVELUP_TEXT_DOMAIN ),
+						'tst1-athr'=> esc_html__( 'CAMERON DOWMAN', LEVELUP_TEXT_DOMAIN ),
+					],
+					[
+						'tst1-image' => esc_html__( 'http://levelup.magazine3.company/wp-content/uploads/2018/10/tm2-3.jpg', LEVELUP_TEXT_DOMAIN ),
+						'tst1-head'=> esc_html__( 'Take a Look at Our Awesome New Product', LEVELUP_TEXT_DOMAIN ),
+						'tst1-desc'=> esc_html__( 'Of course we haven\'t forgotten about the responsive layout. Create a website with full mobile support.', LEVELUP_TEXT_DOMAIN ),
+						'tst1-athr'=> esc_html__( 'BRAIN OAKMAN', LEVELUP_TEXT_DOMAIN ),
+					],
+					[
+						'tst1-image' => esc_html__( 'http://levelup.magazine3.company/wp-content/uploads/2018/10/tm2-2.jpg', LEVELUP_TEXT_DOMAIN ),
+						'tst1-head'=> esc_html__( 'Innovating Themes for the Future', LEVELUP_TEXT_DOMAIN ),
+						'tst1-desc'=> esc_html__( 'Easily change and tweak your content when you need to, however you want, No more third party wendor lock-in.', LEVELUP_TEXT_DOMAIN ),
+						'tst1-athr'=> esc_html__( 'EVAN BARRINGTON ', LEVELUP_TEXT_DOMAIN ),
+					],
+				],
+				'title_field' => 'Repeater',
+				'conditions'=> array(
+					'terms'	=> array(
+						array(
+							'name' => 'layoutDesignSelected',
+							'value' => 'testimonial-design-1',
+						)
+					)
+				)
+			]
+		);
 
 		$this->end_controls_section();
 
